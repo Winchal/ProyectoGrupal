7d223d1b80c1dc2109e3d0bc88791e51c1f4ede3
Winchal <oscar.roldan@educa.madrid.org>
2024-01-19 17:39:50 +0100

    version inicial

---
+++ ProyectoChinchulin.java
@@ -0,0 +1,10 @@
+
+public class ProyectoChinchulin {
+    public static void main(String[] args) {
+    areaTriangulo();
+    areaCuadrado();
+    areaCirculo();
+    areaRectangulo();
+    }
+    
+}
