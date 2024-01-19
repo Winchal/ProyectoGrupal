68b11ebe3d015ae5ea3bf2e0c705c5bb8f06a91c
Javilg10 <javier.leon3@educa.madrid.org>
2024-01-19 18:09:38 +0100

    triangulo

--- ProyectoChinchulin.java
+++ ProyectoChinchulin.java
@@ -1,10 +1,13 @@
 
 public class ProyectoChinchulin {
     public static void main(String[] args) {
-    areaTriangulo();
+    areaTriangulo(12,16);
     areaCuadrado();
     areaCirculo();
     areaRectangulo();
     }
+public static int areaTriangulo (int base, int altura){
+	return base * altura / 2;
+}
     
 }
